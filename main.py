9b8ea930469dc362653eea88f816cf8147854b2b
Andrew Dudley <andrew.sutton.dudley@gmail.com>
2022-02-27 12:04:52 -0500

    Unstable

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route('/')
+def index():
+    return 'Hello World';
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
